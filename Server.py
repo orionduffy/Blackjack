7e0028d695cb06c89f5bafd11409325ba5a5f48b
orionduffy <45501204+orionduffy@users.noreply.github.com>
2023-12-18 16:33:51 -0500

    Added optional player cap
    This allows the host to set an upper limit on new players

--- Server.py
+++ Server.py
@@ -12,7 +12,7 @@ from inputimeout import inputimeout, TimeoutOccurred
 
 # https://stackoverflow.com/questions/287871/how-do-i-print-colored-text-to-the-terminal
 from colorama import init as colorama_init
-from colorama import Fore,Style
+from colorama import Fore, Style
 
 # Much of the socket code is copied partly or fully from https://youtu.be/3QiPPX-KeSc?si=wLAnYlhsHv2Fuqry
 
@@ -41,7 +41,7 @@ def handle_clients(players_list):
         player.conn.close()
 
 
-def handle_late_connections(game_thread, players_list, allow_rejoins, allow_new_joins):
+def handle_late_connections(game_thread, players_list, allow_rejoins, allow_new_joins, hard_cap):
     # https://docs.python.org/3/library/socket.html#timeouts-and-the-accept-method
     server.settimeout(1)
 
@@ -58,35 +58,48 @@ def handle_late_connections(game_thread, players_list, allow_rejoins, allow_new_
 
                         print(f"Player {player.name} at IP {player.address} has rejoined the game")
                         try_send_data(player,
-                                  f"{OUTPUT_HEADER}{Fore.GREEN}You have successfully reconnected "
-                                  f"and will keep your previous progress, "
-                                  f"but the game has already started. "
-                                  f"You will have to wait until the next round starts to play!{Style.RESET_ALL}")
+                                      f"{OUTPUT_HEADER}{Fore.GREEN}You have successfully reconnected "
+                                      f"and will keep your previous progress, "
+                                      f"but the game has already started. "
+                                      f"You will have to wait until the next round starts to play!{Style.RESET_ALL}")
                         break
 
+                """
+                If the player has been found and had their "account" reactivated, 
+                then there is no need to handle a new player joining or them getting rejected,
+                so we skip to the next connection
+                """
                 if found_player:
                     continue
 
-            if allow_new_joins:
+            # This section handles new players trying to join or a rejected connection
+            # Players are allowed to rejoin if the hard cap has been met, but no new players are allowed to join
+            if hard_cap and len(players_list) >= hard_cap:
+                send_data(conn,
+                          f"{OUTPUT_HEADER}{Fore.RED}The game has started without you, "
+                          f"and the maximum number of players has been reached. "
+                          f"Try again later!!{Style.RESET_ALL}")
+                send_data(conn, DISCONNECT_MESSAGE)
+            elif allow_new_joins:
                 pname = base_name + str(len(players_list))
                 new_player = Player(pname, conn, addr, mid_join=True)
                 players_list.append(new_player)
 
                 print(f"Player {new_player.name} at IP {new_player.address} has just joined the game")
                 try_send_data(new_player,
-                          f"{OUTPUT_HEADER}{Fore.GREEN}You have successfully connected, "
-                          f"but the game has already started. "
-                          f"You will have to wait until the next round starts to play!{Style.RESET_ALL}")
+                              f"{OUTPUT_HEADER}{Fore.GREEN}You have successfully connected, "
+                              f"but the game has already started. "
+                              f"You will have to wait until the next round starts to play!{Style.RESET_ALL}")
             elif allow_rejoins:
                 send_data(conn,
-                          f"{OUTPUT_HEADER}{Fore.RED}Game has started without you. "
-                          f"New players are not allowed to join. "
+                          f"{OUTPUT_HEADER}{Fore.RED}The game has started without you, "
+                          f"and new players are not allowed to join. "
                           f"Try again later!!{Style.RESET_ALL}")
                 send_data(conn, DISCONNECT_MESSAGE)
             else:
                 send_data(conn,
-                          f"{OUTPUT_HEADER}{Fore.RED}Game has started without you. "
-                          f"You can't join the game now. "
+                          f"{OUTPUT_HEADER}{Fore.RED}The game has started without you, "
+                          f"and you can't join the game now. "
                           f"Try again later!!{Style.RESET_ALL}")
                 send_data(conn, DISCONNECT_MESSAGE)
         except socket.timeout:
@@ -103,6 +116,13 @@ def start():
         .confirm("Would you like to allow players to rejoin in between games if they disconnected?").ask()
     allow_new_joins = questionary \
         .confirm("Would you like to allow new players to join in between games?").ask()
+    
+    if allow_new_joins:
+        hard_cap = questionary.confirm("Would you like to set a maximum number of players?").ask()
+        if hard_cap:
+            hard_cap = questionary.text("What is the maximum number of players you want to allow to join your game?",
+                                        validate=validate_no_players).ask()
+            hard_cap = int(hard_cap)
 
     print(f"{Fore.GREEN}Waiting for players to join...{Style.RESET_ALL}")
 
@@ -121,16 +141,16 @@ def start():
 
         try:
             pname = receive_data(conn) + str(len(players_list))
-            
+
             new_player = Player(pname, conn, addr)
             print_above(f"Player {new_player.name} has joined")
             msg = OUTPUT_HEADER + f"{Fore.GREEN}{pname}, please wait for the game to start>>>>{Style.RESET_ALL}"
-            
+
             send_data(conn, msg)
             players_list.append(new_player)
         except socket.error as e:
             print_above(f"{Fore.RED}An error occurred with the connection. "
-                  f"Dropping them from the list of connected players{Style.RESET_ALL}")
+                        f"Dropping them from the list of connected players{Style.RESET_ALL}")
             continue
 
         connected_players = list(filter(lambda person: person.connected, players_list))
@@ -152,7 +172,7 @@ def start():
             discmsg = OUTPUT_HEADER + f"{Fore.RED}One or more players disconnected. {Style.RESET_ALL}" \
                                       f"{Fore.BLUE}There are now only {len(connected_players)} players. " \
                                       f"Waiting for {no_players - len(connected_players)} more players to join.{Style.RESET_ALL}"
-  
+
             for player in connected_players:
                 try_send_data(player, discmsg)
 
@@ -170,7 +190,7 @@ def start():
         #     early_thread.daemon = True
         #     early_thread.start()
     early_thread.join()
-    start_game(players_list, allow_rejoin, allow_new_joins)
+    start_game(players_list, allow_rejoin, allow_new_joins, hard_cap)
 
 
 def early_start(should_start, players_list):
@@ -201,18 +221,19 @@ def early_start(should_start, players_list):
             should_start[0] = True
 
 
-def start_game(players_list, allow_rejoin, allow_new_joins):
+def start_game(players_list, allow_rejoin, allow_new_joins, hard_cap):
     pause_game = [False]
     thread = threading.Thread(target=handle_clients, args=(players_list,))
-    thread_late_conn = threading.Thread(target=handle_late_connections, args=(thread,
-                                                                           players_list,
-                                                                           allow_rejoin,
-                                                                           allow_new_joins))
+    thread_late_conn = threading.Thread(target=handle_late_connections,
+                                        args=(thread, players_list, allow_rejoin, allow_new_joins, hard_cap))
+
     print(f"{Fore.GREEN}Game Starting ....{Style.RESET_ALL}")
+
     thread.start()
     thread_late_conn.start()
     thread.join()
     thread_late_conn.join()
+
     print(f"{Fore.GREEN}Game End...Server Shutting down{Style.RESET_ALL}")
 
 
@@ -269,7 +290,7 @@ def print_above(msg, up_amount=1, newline_amount=1):
         - print the message
         - go back to saved position
     """
-    print(f"\x1b[s\x1b[{up_amount}F\x1b[S\x1b[{newline_amount}L"+msg+"\x1b[u", end="",flush=True)
+    print(f"\x1b[s\x1b[{up_amount}F\x1b[S\x1b[{newline_amount}L" + msg + "\x1b[u", end="", flush=True)
 
 
 if __name__ == '__main__':
